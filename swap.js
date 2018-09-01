a0e88e36e6aaf6299d14c846ca2c98e2550f1456
Titin Farida <ltfarida@gmail.com>
2018-08-31 23:48:40 -0500

    add swap.js

---
+++ swap.js
@@ -0,0 +1,13 @@
+var x = 17;
+var y = 50; 
+console.log("x is "+x);
+console.log("y is "+y);
+
+//swapping..
+var temp = x;
+x = y;
+y = temp;
+
+console.log("after swap");
+console.log("x is now "+x);
+console.log("y is now "+y);
